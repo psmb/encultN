b3ede20c8875d6fd32c10452ff816df3ccb969ef
Dmitri Pisarev <dimaip@gmail.com>
2015-10-31 16:33:54 +0300

    Api refactoring + misc

--- app/redux/api.js
+++ app/redux/api.js
@@ -7,11 +7,20 @@ export function voteForAnswerPromise(id) {
   return fetch(ownAddress + '/api/vote-for-answer?answerIdentifier=' + id, { method: 'put', credentials: 'include' }).then(response => response.json()).then(json => fromJS(json)).catch(error => console.error('MIDDLEWARE ERROR:', error));
 }
 export function fetchQuestionsPromise() {
-  return fetch(ownAddress + '/api/' + getLang() + '/' + 'voprosy.json').then(response => response.json()).then(json => fromJS(json)).catch(error => console.error('MIDDLEWARE ERROR:', error));
+  return fetch(ownAddress + '/api/' + getLang() + '/questions.json').then(response => response.json()).then(json => fromJS(json)).catch(error => console.error('MIDDLEWARE ERROR:', error));
 }
 export function fetchAnswersPromise(path) {
-  return fetch(ownAddress + '/api/' + getLang() + '/' + 'voprosy/' + path + '.json').then(response => response.json()).then(json => fromJS(json)).catch(error => console.error('MIDDLEWARE ERROR:', error));
+  return fetch(ownAddress + '/api/' + getLang() + '/questions/' + path + '.json').then(response => response.json()).then(json => fromJS(json)).catch(error => console.error('MIDDLEWARE ERROR:', error));
 }
 export function fetchWorldviewsPromise() {
-  return fetch(ownAddress + '/api/' + getLang() + '/' + 'mirovozzreniya.json').then(response => response.json()).then(json => fromJS(json)).catch(error => console.error('MIDDLEWARE ERROR:', error));
+  return fetch(ownAddress + '/api/' + getLang() + '/worldviews.json').then(response => response.json()).then(json => fromJS(json)).catch(error => console.error('MIDDLEWARE ERROR:', error));
+}
+export function fetchWorldviewPromise(path) {
+  return fetch(ownAddress + '/api/' + getLang() + '/worldviews/' + path + '.json').then(response => response.json()).then(json => fromJS(json)).catch(error => console.error('MIDDLEWARE ERROR:', error));
+}
+export function fetchBlogsPromise() {
+  return fetch(ownAddress + '/api/' + getLang() + '/blogs.json').then(response => response.json()).then(json => fromJS(json)).catch(error => console.error('MIDDLEWARE ERROR:', error));
+}
+export function fetchBlogPromise(path) {
+  return fetch(ownAddress + '/api/' + getLang() + '/blogs/' + path + '.json').then(response => response.json()).then(json => fromJS(json)).catch(error => console.error('MIDDLEWARE ERROR:', error));
 }
