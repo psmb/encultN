f477ba0ca20c7d1b53ee60b0805fa973d47a7d6e
Dmitri Pisarev <dimaip@gmail.com>
2015-11-11 11:57:25 +0300

    Adjust icons a little

--- app/components/Questions/QuestionSmall.jsx
+++ app/components/Questions/QuestionSmall.jsx
@@ -15,18 +15,14 @@ export default class QuestionSmall extends Component {
     return (
       <Link key={this.props.question.get('id')} className={this.props.question.get('votedAnswer') ? 'QuestionSmall QuestionSmall--voted' : 'QuestionSmall'} to={`/q/${this.props.question.get('id')}`}>
         <div className='QuestionSmall-wrap'>
-          <div className='QuestionSmall-content'>
-            <h2 className='mdl-typography--headline-color-contrast QuestionSmall-title'>{this.props.question.get('title')}</h2>
-            <h3 className='mdl-typography--body-1-color-contrast QuestionSmall-subTitle'>{this.props.question.get('subTitle')}</h3>
-            <p className='mdl-typography--caption-color-contrast QuestionSmall-lead'>
-            <span className='color-primary'>{this.props.question.get('voteCount')}<i className='icon-check'></i></span>
-              • {__('Лидирует')} <span className='color-primary'>{this.props.question.get('worldviewWithMaxVotes')}: {this.props.question.get('answerWithMaxVotesCount')}<i className='icon-check'></i></span>
-            </p>
-          </div>
-          <div className='QuestionSmall-votes'>
-          </div>
+          <h2 className='mdl-typography--headline-color-contrast QuestionSmall-title'>{this.props.question.get('title')}</h2>
+          <h3 className='mdl-typography--body-1-color-contrast QuestionSmall-subTitle'>{this.props.question.get('subTitle')}</h3>
+          <p className='mdl-typography--caption-color-contrast QuestionSmall-lead'>
+          {__('Всего')}: <span className='color-primary'>{this.props.question.get('voteCount')}<i className='icon-check'></i></span>
+            • {this.props.question.get('worldviewWithMaxVotes')}: <span className='color-primary'>{this.props.question.get('answerWithMaxVotesCount')}<i className='icon-check'></i></span>
+          </p>
         </div>
-        <div className='QuestionSmall-questionMark hide-for-small-only mdl-shadow--4dp'>
+        <div className='QuestionSmall-icon hide-for-small-only mdl-shadow--4dp'>
           <i className='icon-right-circle'></i>
         </div>
       </Link>
