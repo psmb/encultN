32e11be49fb3951318359473cc314f8c3c37f6af
Dmitri Pisarev <dimaip@gmail.com>
2015-11-13 00:59:00 +0300

    About page

--- app/components/About/About.jsx
+++ app/components/About/About.jsx
@@ -1,16 +1,17 @@
-import React, {Component, PropTypes} from 'react';
+import React, {Component} from 'react';
 
 export default class About extends Component {
-  static propTypes = {
-    question: PropTypes.object.isRequired,
-  }
-
   render() {
     return (
-      <div className='About marginTop-double'>
+      <div className='About marginTop-double fixed-width'>
         <div className='row'>
-          <div className='medium-10 medium-offset-1 large-8 large-offset-2 columns fixed-width'>
+          <div className='medium-8 columns'>
             <h1 className='marginVertical-double mdl-typography--display-1-color-contrast'>О проекте</h1>
+            <p className='mdl-typography--body-1-color-contrast'>Проект осуществляется автономной некоммерческой организацией "Центр изучения и развития межкультурных отношений"</p>
+          </div>
+        </div>
+        <div className='row'>
+          <div className='medium-10 medium-offset-1 columns'>
             <h2 className='marginTop-double marginBottom-single mdl-typography--headline-color-contrast'>Как это работает</h2>
             <p className='mdl-typography--body-1-color-contrast'>Аутентичные представители различных духовных движений коротко отвечают на жизненно важные вопросы, раскрывая специфику своих взглядов на мир.</p>
             <p className='mdl-typography--body-1-color-contrast'>Вы выбираете ответ вслепую, не зная, к какому именно мировоззрению, движению, течению относится его автор. Только сделав выбор, вы узнаёте, что вы выбрали.</p>
