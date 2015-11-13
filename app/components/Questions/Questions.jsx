9b97b0426d1ababfe6448a5cdf9009d291957df2
Dmitri Pisarev <dimaip@gmail.com>
2015-11-14 00:37:01 +0300

    Minor fixes

--- app/components/Questions/Questions.jsx
+++ app/components/Questions/Questions.jsx
@@ -35,10 +35,14 @@ export default class Questions extends Component {
             <QuestionsList questions={this.props.questions}/>
           </div>
         </div>
-        <div className='paddingTop-triple marginTop-triple'>
-          <p className='mdl-typography--caption color-primary Hint textAlign-center'>
-            {__('Здесь вы можете сделать пожертвование на этот и другие пообные проекты, способствующие изучению и развитию межкультурных отношений.')}
-          </p>
+        <div className='row'>
+          <div className='medium-8 medium-offset-2 columns'>
+            <div className='paddingTop-triple marginTop-triple'>
+              <p className='mdl-typography--caption color-primary Hint textAlign-center'>
+                {__('Здесь вы можете сделать пожертвование на этот и другие подобные проекты, способствующие изучению и развитию межкультурных отношений.')}
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     );
