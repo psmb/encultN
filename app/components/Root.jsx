0df02402af984d26d3c9b37de5e04cf603c05c62
Dmitri Pisarev <dimaip@gmail.com>
2015-11-13 10:52:14 +0300

    Mobile nav

--- app/components/Root.jsx
+++ app/components/Root.jsx
@@ -11,34 +11,64 @@ export default class Root extends React.Component {
     location: React.PropTypes.object,
   }
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      mobileNavOpen: false,
+    };
+  }
+
+  toggleMenu = () => {
+    this.setState({
+      mobileNavOpen: !this.state.mobileNavOpen,
+    });
+  }
+
   render() {
     const __ = puttext();
-    const linkEn = <LinkR to={this.props.location.pathname.replace('/ru', '/en')}>English</LinkR>;
-    const linkRu = <LinkR to={this.props.location.pathname.replace('/en', '/ru')}>Russian</LinkR>;
+    const linkEn = <LinkR className='MenuItem' to={this.props.location.pathname.replace('/ru', '/en')}>English</LinkR>;
+    const linkRu = <LinkR className='MenuItem' to={this.props.location.pathname.replace('/en', '/ru')}>Russian</LinkR>;
+    const menu = (
+      <div>
+        <Link className='MenuItem' to={`/about`}>{__('О проекте')}</Link>
+        <Link className='MenuItem' to={`/stats`}>{__('Статистика')}</Link>
+        <Link className='MenuItem' to={`/analytics`}>{__('Аналитика')}</Link>
+        {getLang() === 'ru' ? linkEn : linkRu}
+      </div>
+    );
     return (
-      <div className=''>
-        <div className='fixed-width'>
-          <header className='Header'>
-            <div className='columns'>
-              <Link className='Header-logo' to={``}><img src={'/static/logo-' + getLang() + '.svg'} /></Link>
-              <div className='hide-for-small-only float-right Menu'>
-                <Link to={`/about`}>{__('О проекте')}</Link>
-                <Link to={`/stats`}>{__('Статистика')}</Link>
-                <Link to={`/analytics`}>{__('Аналитика')}</Link>
-                {getLang() === 'ru' ? linkEn : linkRu}
+      <div className={this.state.mobileNavOpen ? 'MobileNav isActive' : 'MobileNav'}>
+        <div className='MobileNav-Content'>
+          <div className='fixed-width'>
+            <header className='Header'>
+              <div className='columns'>
+                <Link className='Header-logo' to={``}><img src={'/static/logo-' + getLang() + '.svg'} /></Link>
+                <div onClick={this.toggleMenu} className='show-for-small-only MobileNav-Toggle'>
+                  <div className='IconMenu'>
+                    <svg xmlns='http://www.w3.org/2000/svg' width='24px' height='24px' viewBox='0 0 32 32'>
+                      <path d='M4,10h24c1.104,0,2-0.896,2-2s-0.896-2-2-2H4C2.896,6,2,6.896,2,8S2.896,10,4,10z M28,14H4c-1.104,0-2,0.896-2,2  s0.896,2,2,2h24c1.104,0,2-0.896,2-2S29.104,14,28,14z M28,22H4c-1.104,0-2,0.896-2,2s0.896,2,2,2h24c1.104,0,2-0.896,2-2  S29.104,22,28,22z'/>
+                    </svg>
+                  </div>
+                </div>
+                <div className='hide-for-small-only float-right Menu'>
+                  {menu}
+                </div>
               </div>
-            </div>
-          </header>
-        </div>
-        <main className='Main'>
-          {this.props.children}
-        </main>
-        <footer className='Footer'>
-          <div className='Footer-inner fixed-width'>
-            © {__('Мониторинг мировоззрений')}<br/>
-            {__('Дизайн и разработка:')} <a href='http://sfi.ru'>{__('веб-служба Свято-Филаретовского православного-христианского института')}</a>
+            </header>
           </div>
-        </footer>
+          <main className='Main'>
+            {this.props.children}
+          </main>
+          <footer className='Footer'>
+            <div className='Footer-inner fixed-width'>
+              © {__('Мониторинг мировоззрений')}<br/>
+              {__('Дизайн и разработка:')} <a href='http://sfi.ru'>{__('веб-служба Свято-Филаретовского православного-христианского института')}</a>
+            </div>
+          </footer>
+        </div>
+        <div className='MobileNav-Menu show-for-small-only'>
+          {menu}
+        </div>
       </div>
     );
   }
